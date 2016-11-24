535df6f11da20e643b995ecfd156b4caf933b2d2
Franz Ulrich <franz.ulrich.cr@gmail.com>
2016-11-24 16:20:44 -0600

    feat: Added fake data to ListView
    Data for the ListView was created using a String array, an ArrayList and
    an Adapter in the Fragment class.
    
    End of lesson 1.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -56,7 +61,34 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //Populate ListView with hardcoded fake data
+
+            //Create an array of Strings with the hardcoded data items
+            String[] strFakeDataArray = {
+                    "Hoy, lluvia, 23º / 18º",
+                    "Mañana, tormentas, 25º/ 18º",
+                    "Sábado, chubascos, 25º/ 19º",
+                    "Domingo, tormentas, 24º/ 18º",
+                    "Lunes, tormentas, 25º/ 17º",
+                    "Martes, tormentas, 25º/ 17º"
+            };
+            //Create a String ArrayList and pass StringArray as a list
+            List<String> strLstForecastData = new ArrayList<String>(Arrays.asList(strFakeDataArray));
+            //Create ArrayAdapter with Strings
+            ArrayAdapter<String> arrAdpForecastData = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    strLstForecastData);
+           //Find ListView's ID
+            ListView lstVwWeekForecast = (ListView)  rootView.findViewById(R.id.listview_forecast);
+            //ListView lstVwWeekForecast = (ListView)container.findViewById(R.id.listview_forecast);
+            //Attach ArrayAdapter to ListView control
+            lstVwWeekForecast.setAdapter(arrAdpForecastData);
+
             return rootView;
         }
     }
