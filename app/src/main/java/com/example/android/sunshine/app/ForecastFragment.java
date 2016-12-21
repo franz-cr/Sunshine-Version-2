b2987d275b0d30869ddfd2154c02b3f7eda0a8fb
Franz Ulrich <franz.ulrich.cr@gmail.com>
2016-12-21 16:06:59 -0600

    refactor: Moved methods from fragment class to AsyncTask class
    Moved 'parseJsonString', 'getForecastDay', 'simulateJsonString' and 'tempRoundUpDown' methods to FetchWeatherTask class. Also created 'buildForecastDay' method.
    
    Quiz: JSON Parsing.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -23,9 +23,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by ulrichca on 25/11/2016.
@@ -34,45 +38,10 @@ import java.util.List;
 public class ForecastFragment extends android.support.v4.app.Fragment {
 
     //VARIABLES GLOBALES
-    private ArrayAdapter<String> arrAdpForecastData;
+    protected ArrayAdapter<String> arrAdpForecastData;
 
     //FUNCIONES DE LA CLASE
     public ForecastFragment() {
-    }
-
-    public void parseJsonString(String strJsonForecast) {
-        //VARIABLES:
-        final String LOG_TAG = ForecastFragment.class.getSimpleName();
-        Double dblMaxTemp, dblMinTemp, dlbDayTemp;
-        Integer intDayIndex = 0;
-
-        //FUNCIONES:
-        try {
-            //Declare JSON Object and pass string with JSON data
-            JSONObject objJsonForecast = new JSONObject(strJsonForecast);
-            //Declare JSON data array
-            JSONArray arrJsonForecast = objJsonForecast.getJSONArray("list");
-            //Extract into a new JSON object first day forecast data:
-            JSONObject objJsonDayForecast = arrJsonForecast.getJSONObject(intDayIndex);
-            //Extract into new JSON object day temperatures
-            JSONObject objJsonDayTemps = objJsonDayForecast.getJSONObject("temp");
-            //Extract into strings day's temp data
-            dblMinTemp = objJsonDayTemps.getDouble("min");
-            dlbDayTemp = objJsonDayTemps.getDouble("day");
-            dblMaxTemp = objJsonDayTemps.getDouble("max");
-
-            //return dblMaxTemp;
-            //
-            //ListView lstVwWeekForecast = (ListView) R.id.listview_forecast;
-        }
-        catch (JSONException e) {
-            Log.e(LOG_TAG, e.toString());
- /**        //Toast exception error:
-            Toast.makeText(getApplicationContext(),
-                    "Json parsing error: " + e.getMessage(),
-                    Toast.LENGTH_LONG).show();
-                    */
-        }
     }
 
     @Override
@@ -95,13 +64,24 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
     public boolean onOptionsItemSelected(MenuItem fragmentItem) {
         //VARIABLES:
         String strPostalCode = "94043";
+        String[] _strDayData = new String[6];
+        boolean blnJSONsimulate = true;
+
+        //FUNCIONES:
         //Handle item selection
         switch (fragmentItem.getItemId()) {
             case R.id.action_refresh:
-                //Implementation for the 'Refresh' button
                 FetchWeatherTask weatherRefreshTask = new FetchWeatherTask();
                 weatherRefreshTask.execute(strPostalCode);
-                parseJsonString(weatherRefreshTask.strJsonForecast);
+/*                if (blnJSONsimulate)
+                    _strDayData[0] = getForecastDayName(0);
+                    //_strDayData = parseJsonString(simulateJsonString(), 0);
+                else {
+                    //Implementation for the 'Refresh' button
+                    //Passing JSON  string to parsing function while 'strJsonForecast' string
+                    //is still available in memory.
+                    _strDayData = parseJsonString(weatherRefreshTask.strJsonForecast, 0);
+                } */
                 return true;
             default:
                 return super.onOptionsItemSelected(fragmentItem);
@@ -139,31 +119,153 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
         //Attach ArrayAdapter to ListView control
         lstVwWeekForecast.setAdapter(arrAdpForecastData);
 
-
         return rootView;
     }
 
 
     public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
 
-        //VARIABLES GLOBALES DE CLASE
+        //VARIABLES GLOBALES DE SUBCLASE
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-        private final String STR_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
         final String STR_QUERY_PARAM = "q";
         final String STR_MODE_PARAM = "mode";
         final String STR_UNITS_PARAM = "units";
         final String STR_DAYS_PARAM = "cnt";
         final String STR_APPID_PARAM = "APPID";
         private Uri UriBaseUrl = Uri.parse("http://api.openweathermap.org/data/2.5/forecast/daily");
+        private Integer intForecastDays = 7;
         private String strMode = "json";
         private String strUnits = "metric";
-        private String strCount = "7";
+        private String strCount = intForecastDays.toString();
         private String strAppID = "5d7c1591368cffcc1624a4f0e2b5b630";
         private Uri.Builder uriOpenWeatherMap = new Uri.Builder();
+        // Will contain the raw JSON response as a string.
         public String strJsonForecast;
+
+        public String tempRoundUpDown (Double dblTemperature, Boolean blnRoundUp) {
+            //VARIABLES E INICIALIZACIONES:
+
+            //FUNCIONES:
+            if (blnRoundUp)
+                dblTemperature = Math.ceil(dblTemperature);
+            else
+                dblTemperature = Math.floor(dblTemperature);
+
+            return dblTemperature.toString();
+        }
+
+        protected String simulateJsonString () {
+            return "{\"city\":{\"id\":5375480,\"name\":\"Mountain View\",\"coord\":{\"lon\":-122.083847,\"lat\":37.386051},\"country\":\"US\",\"population\":0},\"cod\":\"200\",\"message\":0.3816,\"cnt\":7,\"list\":[{\"dt\":1480705200,\"temp\":{\"day\":13.15,\"min\":5.48,\"max\":14.65,\"night\":5.48,\"eve\":12.65,\"morn\":9.49},\"pressure\":997.17,\"humidity\":53,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":3.01,\"deg\":359,\"clouds\":0},{\"dt\":1480791600,\"temp\":{\"day\":11.38,\"min\":0.37,\"max\":16.15,\"night\":3.14,\"eve\":14.74,\"morn\":0.75},\"pressure\":1000.21,\"humidity\":67,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":1.24,\"deg\":41,\"clouds\":0},{\"dt\":1480878000,\"temp\":{\"day\":10.58,\"min\":-0.12,\"max\":15.4,\"night\":8.21,\"eve\":13.78,\"morn\":0.48},\"pressure\":996.01,\"humidity\":68,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"clear sky\",\"icon\":\"01d\"}],\"speed\":0.39,\"deg\":24,\"clouds\":0},{\"dt\":1480964400,\"temp\":{\"day\":10.37,\"min\":8.59,\"max\":11.51,\"night\":9.84,\"eve\":11.51,\"morn\":8.59},\"pressure\":1013.19,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":5.41,\"deg\":334,\"clouds\":0},{\"dt\":1481054400,\"temp\":{\"day\":11.18,\"min\":7.23,\"max\":11.87,\"night\":7.23,\"eve\":11.87,\"morn\":8.62},\"pressure\":1014.36,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":2.21,\"deg\":314,\"clouds\":55,\"rain\":1.43},{\"dt\":1481140800,\"temp\":{\"day\":9.22,\"min\":5.17,\"max\":9.98,\"night\":9.11,\"eve\":9.98,\"morn\":5.17},\"pressure\":1019.41,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":3.26,\"deg\":150,\"clouds\":88,\"rain\":1.72},{\"dt\":1481227200,\"temp\":{\"day\":10.82,\"min\":9.24,\"max\":12.67,\"night\":12.67,\"eve\":12.63,\"morn\":9.24},\"pressure\":1014.78,\"humidity\":0,\"weather\":[{\"id\":502,\"main\":\"Rain\",\"description\":\"heavy intensity rain\",\"icon\":\"10d\"}],\"speed\":8.29,\"deg\":170,\"clouds\":100,\"rain\":27.67}]}";
+        }
+
+        protected String buildForecastDay(int intDayIndex, String strCloudForecast, double dblMaxTemp,
+                                          double dblMinTemp) {
+            //VARIABLES E INICIALIZACIONES:
+            StringBuilder strBldDayForecast = new StringBuilder();
+            String strDayForecast = "";
+
+            //PROCESO:
+
+            //Get date in long format
+            strBldDayForecast.append(getForecastDayName(intDayIndex));
+            strBldDayForecast.append("; ");
+
+            //Load clouds status data
+            strBldDayForecast.append(strCloudForecast);
+
+            //Load day data array with the JSON extracted data
+            strBldDayForecast.append(tempRoundUpDown(dblMaxTemp, Boolean.TRUE));
+            strBldDayForecast.append("º/ ");
+            strBldDayForecast.append(tempRoundUpDown(dblMinTemp, Boolean.FALSE));
+            strBldDayForecast.append("º");
+
+            strDayForecast = strBldDayForecast.toString();
+
+            return strDayForecast;
+        }
+
+        protected String getForecastDayName(int intDayIndex) {
+            //VARIABLES E INICIALIZACIONES:
+            String strForecastDayName = "";
+            Calendar calForecastDay = Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault());
+            SimpleDateFormat dtFmtShortDayDate = new SimpleDateFormat("EEEE, MMMM dd, yyyy",
+                    Locale.getDefault());
+
+            //PROCESO:
+            calForecastDay.add(Calendar.DAY_OF_YEAR,intDayIndex);
+            strForecastDayName = dtFmtShortDayDate.format(calForecastDay.getTime());
+
+            return strForecastDayName;
+        }
+
+        public String[] parseJsonString(Integer intDaysCount) {
+            //VARIABLES E INICIALIZACIONES:
+            final String LOG_TAG = ForecastFragment.class.getSimpleName();
+            String[] strDaysFormattedData = new String[intDaysCount];
+            Integer intHumidity, intWeatherID, intClouds, intDegrees;
+            Long lngUnixDate;
+            Double dblMaxTemp, dblMinTemp, dblDayTemp, dblNightTemp, dblMorningTemp, dblEveningTemp,
+                    dblPressure, dblWindSpeed;
+            String strWeatherMain, strWeatherDescription, strWeatherIcon;
+
+            //PROCESO:
+            try {
+                //Declare JSON Object and pass string with JSON data
+                JSONObject objJsonForecast = new JSONObject(strJsonForecast);
+                //Declare JSON data array
+                JSONArray arrJsonForecast = objJsonForecast.getJSONArray("list");
+
+                //Parse retrieved days
+                for (int i = 0; i < intDaysCount; i++) {
+                    //Extract into a new JSON object first day forecast data:
+                    JSONObject objJsonDayForecast = arrJsonForecast.getJSONObject(i);
+
+                    //Extract data of selected day
+                    lngUnixDate = objJsonDayForecast.getLong("dt");
+                    dblPressure = objJsonDayForecast.getDouble("pressure");
+                    intHumidity = objJsonDayForecast.getInt("humidity");
+                    dblWindSpeed = objJsonDayForecast.getDouble("speed");
+                    intDegrees = objJsonDayForecast.getInt("deg");
+                    intClouds = objJsonDayForecast.getInt("clouds");
+
+                    //Extract into new JSON object day's temperatures data
+                    JSONObject objJsonDayTemps = objJsonDayForecast.getJSONObject("temp");
+                    //Extract requested day temps data from temps object
+                    dblDayTemp = objJsonDayTemps.getDouble("day");
+                    dblMinTemp = objJsonDayTemps.getDouble("min");
+                    dblMaxTemp = objJsonDayTemps.getDouble("max");
+                    dblNightTemp = objJsonDayTemps.getDouble("night");
+                    dblEveningTemp = objJsonDayTemps.getDouble("eve");
+                    dblMorningTemp = objJsonDayTemps.getDouble("morn");
+
+                    //Extract into a JSON array day's first weather data
+                    JSONArray arrJsonWeather = objJsonDayForecast.getJSONArray("weather");
+                    JSONObject objJsonDayWeather = arrJsonWeather.getJSONObject(0);
+                    intWeatherID = objJsonDayWeather.getInt("id");
+                    strWeatherMain = objJsonDayWeather.getString("main");
+                    strWeatherDescription = objJsonDayWeather.getString("description");
+                    strWeatherIcon = objJsonDayWeather.getString("icon");
+
+                    //Load String array with day's forecast data
+                    strDaysFormattedData[i] = buildForecastDay(i, strWeatherDescription, dblMaxTemp,
+                            dblMinTemp);
+                }
+            }
+            catch (JSONException e) {
+                Log.e(LOG_TAG, e.toString());
+                /**        //Toast exception error:
+                 Toast.makeText(getApplicationContext(),
+                 "Json parsing error: " + e.getMessage(),
+                 Toast.LENGTH_LONG).show();
+                 */
+                strDaysFormattedData[0] = "ERROR";
+            }
+            return strDaysFormattedData;
+        }
 
         @Override
         protected Void doInBackground(String... strPostalCode) {
+            //TODO: Cambiar tipo del método 'Void' a 'String[]'
 
             //VARIABLES GLOBALES DE FUNCIÓN
             // These two need to be declared outside the try/catch
@@ -171,30 +273,22 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
+            //PROCESO:
+            //Checking that postal code array is not empty (has at least 1 element)
+            if (strPostalCode.length == 0)
+                return null;
+
+            //TODO: Delete or comment this sentence once parse string unit test is passed.
+            //Command added just for unit testing parse method.
+            if (strJsonForecast.length() > 0) {
+                parseJsonString(intForecastDays);
+                return null;
+            }
 
             try {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-
-                // Initial approach: manual hardcoded URL
-                //String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
-                //        strPostalCode[0] + "&mode=json&units=metric&cnt=7";
-                //String appKey = "&APPID=5d7c1591368cffcc1624a4f0e2b5b630";
-                //String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
-                //Pass hardcoded string Url to URL object
-                //URL urlOpenWeatherMap = new URL(baseUrl.concat(appKey));
-
-                // Best approach: using Uri and Uri.Builder classes to build the URL.
-                uriOpenWeatherMap = UriBaseUrl.buildUpon();
-                uriOpenWeatherMap.appendQueryParameter(STR_QUERY_PARAM, strPostalCode[0]);
-                uriOpenWeatherMap.appendQueryParameter(STR_MODE_PARAM, strMode);
-                uriOpenWeatherMap.appendQueryParameter(STR_UNITS_PARAM, strUnits);
-                uriOpenWeatherMap.appendQueryParameter(STR_DAYS_PARAM, strCount);
-                uriOpenWeatherMap.appendQueryParameter(STR_APPID_PARAM, strAppID);
-
                 uriOpenWeatherMap = UriBaseUrl.buildUpon()
                     .appendQueryParameter(STR_QUERY_PARAM, strPostalCode[0])
                     .appendQueryParameter(STR_MODE_PARAM, strMode)
@@ -203,7 +297,6 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
                     .appendQueryParameter(STR_APPID_PARAM, strAppID);
                 //Pass built Uri to URL object
                 URL urlOpenWeatherMap = new URL(uriOpenWeatherMap.build().toString());
-
 
                 //Prints out into the log the query URL
                 Log.i(LOG_TAG + " - urlOpenWeatherMap", urlOpenWeatherMap.toString());
@@ -234,10 +327,8 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                //Capturing the JSON string
-                forecastJsonStr = buffer.toString();
 
-                //Passing the JSON string to Global string variable
+                //Capturing the JSON string to Global string variable
                 strJsonForecast = buffer.toString();
 
                 //Prints into the log the returned Json string
@@ -260,63 +351,18 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
                     }
                 }
             }
+
+            //TODO: Agregar la llamada al método 'parseJsonString' dentro de un 'try/catch'.
+
+            //TODO Ingresar un 'return' con el arreglo de strings proveniente de 'parseJsonString'.
+
             return null;
         }
 
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
-
-    /*
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            //http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=5d7c1591368cffcc1624a4f0e2b5b630
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=5d7c1591368cffcc1624a4f0e2b5b630");
-
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                return null;
-            }
-            forecastJsonStr = buffer.toString();
-        } catch (IOException e) {
-            Log.e("ForecastFragment", "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attemping
-            // to parse it.
-            return null;
-        } finally{
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e("ForecastFragment", "Error closing stream", e);
-                }
-            }
-        }
-    */
 }
 
